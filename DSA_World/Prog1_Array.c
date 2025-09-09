20bf892fd0dee060d789b29c9a66baedefc6b32a
Kishan74 <kishanchandrachaurasia727575@gmail.com>
2025-09-09 17:00:46 +0530

    added the first program

---
+++ DSA_World/Prog1_Array.c
@@ -0,0 +1,16 @@
+// Array Introduction
+
+#include<iostream>
+#include<stdio.h>
+using namespace std;
+int main()
+{
+    int A[5];
+     A[0]=10;
+     A[1]=12;
+     A[2]=14;
+    
+    std::cout << sizeof(A) << std::endl;
+    cout<<A[1]<< endl;
+    printf("%d\n",A[2]);
+}
