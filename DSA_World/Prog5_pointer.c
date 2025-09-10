b6401243d88b2a46f79c6c9dba0a93620cbf063a
Kishan Chandra Chaurasia <69727798+kishanchandrachaurasia74@users.noreply.github.com>
2025-09-10 10:45:55 +0530

    Create Prog5_pointer.c
    Added the pointer program with malloc function

---
+++ DSA_World/Prog5_pointer.c
@@ -0,0 +1,24 @@
+#include <iostream>
+#include<stdio.h>
+#include<stdlib.h>
+
+using namespace std;
+
+int main()
+{
+    int *p;
+    p=(int *)malloc(5*sizeof(int));
+    
+    p[0]=10;
+    p[1]=15;
+    p[2]=23;
+    p[3]=12;
+    p[4]=34;
+    
+    
+    for(int i =0;i<5;i++)
+    {
+        printf("%d\n",p[i]);
+    }
+    
+}
