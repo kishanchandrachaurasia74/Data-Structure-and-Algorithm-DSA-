525e560fe33eaaad352814962a22b10387ef7074
Kishan <kishanchandrachaurasia727575@gmail.com>
2026-01-03 12:42:39 +0530

    added the Taylor series Horner's rule program

---
+++ DSA_World/Prog35_TaylorSeriesHorners.c
@@ -0,0 +1,20 @@
+//Taylor Series Using Horner's Rule
+
+#include <stdio.h>
+
+double  e(int x,int n)
+{
+    static double s;
+    
+    if(n==0)
+        return s;
+    s=1+x*s/n;
+    return e(x,n-1);
+}
+
+int main()
+{
+    
+    printf("%lf",e(4,15));
+    return 0;
+}
